94418f699095570de1b4790c506b061d7394834b
Deon van Vuuren <61790571+Deon-v@users.noreply.github.com>
2022-06-27 23:05:09 +0100

    Init

---
+++ src/Client/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MyProfile.Client;
+
+namespace Company.WebApplication1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddHttpClient("MyProfile.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+            // Supply HttpClient instances that include access tokens when making requests to the server project
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("MyProfile.ServerAPI"));
+
+            builder.Services.AddMsalAuthentication(options =>
+            {
+                builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("api://589bd583-3ab8-41e4-853d-0a0b7447e9e6/API.Access");
+                options.ProviderOptions.LoginMode = "redirect";
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
